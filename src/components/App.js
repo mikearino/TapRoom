68334c9d1b592747890d91338b997bcc5b9b6045
Mike Lofgren <mikeylofgren@gmail.com>
2019-09-06 12:09:03 -0700

    work on hash router

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import { Row, Col } from 'react-bootstrap';
 import Footer from './Footer';
 import { Switch, Route } from 'react-router'
 
+
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -6,7 +6,16 @@ import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <HashRouter>
+        <Component/>
+      </HashRouter>
+    </AppContainer>,
+    document.getElementById('react-app-root')
+  );
+};
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
