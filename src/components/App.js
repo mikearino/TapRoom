a4048592d6d31d1cc56107a437ba9e1c733403df
Mike Lofgren <mikeylofgren@gmail.com>
2019-09-06 09:40:39 -0700

    work on components

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import '../App.css';
-
+import Navigation from './Navigation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-          Learn React
-
-      </header>
+      <Navigation/>
     </div>
   );
 }
