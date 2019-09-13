44326d246bb0a94b5b07521b1867290107d761ca
Mike Lofgren <mikeylofgren@gmail.com>
2019-09-13 11:30:28 -0700

    deal with state

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../App.css';
 import { Switch, Route } from 'react-router-dom';
 import Homepage from './Homepage'
-import NewKegForm from './NewKegForm'
+import NewKegControl from './NewKegControl'
 import Error404 from './Error404';
 import KegList from './KegList'
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <Switch>
       <Route exact path='/' component={KegList}/>
-      <Route path='/NewKegForm' component={NewKegForm}/>
+      <Route path='/newkeg' component={NewKegControl}/>
       <Route component={Error404} />
     </Switch>
   );
