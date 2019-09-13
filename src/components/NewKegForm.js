45dce4c39ad5e5dbd070dbe2a139350098f16a84
Mike Lofgren <mikeylofgren@gmail.com>
2019-09-13 13:16:59 -0700

    work on form

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -4,6 +4,13 @@ import { Row, Col, Button, Form } from 'react-bootstrap';
 
 
 function NewKegForm() {
+
+  let _name = null;
+  let _brand = null;
+  let _price = null;
+  let _abv = null;
+  let _flavor = null;
+
   var tapStyles = {
     color: 'white',
     background: 'black',
@@ -12,28 +19,43 @@ function NewKegForm() {
     border: '3px solid green',
     padding: '10px'
   }
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    console.log(_name);
+    console.log(_brand);
+    console.log(_price);
+    console.log(_abv);
+    console.log(_flavor);
+  }
+
   return (
     <div>
-    <form>
+    <form onSubmit={handleNewKegFormSubmission}>
       <input
       type='text'
       id='name'
-      placeholder='Name'/>
+      placeholder='Name'
+      ref={(input) => {_name = input;}}/>
       <input
       type='text'
       id='brand'
-      placeholder='Brand'/>
+      placeholder='Brand'
+      ref={(input) => {_brand = input;}}/>
       <input
       type='text'
       id='price'
-      placeholder='Price'/>
+      placeholder='Price'
+      ref={(input) => {_price = input;}}/>
       <input
       type='text'
       id='abv'
-      placeholder='ABV'/>
+      placeholder='ABV'
+      ref={(input) => {_abv = input;}}/>
       <textarea
       id='flavor'
-      placeholder='Flavor profile'/>
+      placeholder='Flavor profile'
+      ref={(textarea) => {_name = textarea;}}/>
       <button type='submit'>Submit!</button>
     </form>
     </div>
